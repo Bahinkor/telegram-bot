85f9c30fdfb424ccb9343b0ec4dc0b61854bf980
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-10-24 20:02:00 +0330

    Edit app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const {Telegraf} = require("telegraf");
 const dotenv = require("dotenv");
 const startCommand = require("./commands/start.command");
@@ -15,20 +16,33 @@ const jokeCommand = require("./commands/joke.command");
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start(startCommand);
-bot.help(helpCommand);
-bot.action("addToGroup", addToGroupCommand);
-bot.on("new_chat_members", newGroupWelcomeMessageCommand);
-bot.hears(/^\/crypto .*/i, getCryptoMoneyCommand);
-bot.hears(/^\/fiat .*/i, getFiatMoneyCommand);
-bot.hears(/\/time/i, dateCommand);
-bot.hears(/^\/echo .*/i, echoCommand);
-bot.hears(/^\/meme$/i, memeCommand);
-bot.hears(/^\/joke( programming)?/i, jokeCommand);
-bot.on("text", garbageCommand);
+const server = http.createServer(() => {
+    try {
+        bot.start(startCommand);
+        bot.help(helpCommand);
+        bot.action("addToGroup", addToGroupCommand);
+        bot.on("new_chat_members", newGroupWelcomeMessageCommand);
+        bot.hears(/^\/crypto .*/i, getCryptoMoneyCommand);
+        bot.hears(/^\/fiat .*/i, getFiatMoneyCommand);
+        bot.hears(/\/time/i, dateCommand);
+        bot.hears(/^\/echo .*/i, echoCommand);
+        bot.hears(/^\/meme$/i, memeCommand);
+        bot.hears(/^\/joke( programming)?/i, jokeCommand);
+        bot.on("text", garbageCommand);
 
-bot.launch()
-    .then(() => console.log("Bot is running..."))
-    .catch(err => `Bot failed to start, error => ${err}`);
+        bot.launch()
+            .then(() => console.log("Bot is running..."))
+            .catch(err => `Bot failed to start, error => ${err}`);
 
-bot.catch(err => console.log(`Bot error => ${err}`));
+        bot.catch(err => console.log(`Bot error => ${err}`));
+
+    } catch (err) {
+        console.log(`server error => ${err}`);
+    }
+});
+
+server.listen(8000, () => {
+    console.log("server started on port 8000");
+});
+
+server();
